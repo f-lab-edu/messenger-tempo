611057d89ceb47636e54fb422f68ee324b122673
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-02-01 21:19:24 +0900

    generated by start.spring.io

---
+++ src/main/java/com/messenger/MessengerTempoApplication.java
@@ -0,0 +1,13 @@
+package com.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerTempoApplication.class, args);
+	}
+
+}
