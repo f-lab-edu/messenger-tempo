6104097682ae8f9be889f60a0b62732e733bf449
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-02-03 18:41:49 +0900

    print Spring version in console

--- src/main/java/com/messenger/MessengerTempoApplication.java
+++ src/main/java/com/messenger/MessengerTempoApplication.java
@@ -2,12 +2,16 @@ package com.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class MessengerTempoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessengerTempoApplication.class, args);
+
+		String getVersion = SpringVersion.getVersion();
+		System.out.println("Spring version = "+getVersion);
 	}
 
 }
