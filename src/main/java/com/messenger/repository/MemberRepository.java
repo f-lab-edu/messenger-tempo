16401762b6b9eb1d8fd9f2304ff1b727163048cc
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-02-13 19:34:27 +0900

    add method: findByName

--- src/main/java/com/messenger/repository/MemberRepository.java
+++ src/main/java/com/messenger/repository/MemberRepository.java
@@ -8,6 +8,7 @@ public interface MemberRepository {
 
     public boolean save(Member member);
     public Optional<Member> findById(String id);
+    public List<Member> findByName(String name);
     public List<Member> findAll();
     public boolean updatePassword(String id, String password);
     public boolean updateDisplayName(String id, String name);
