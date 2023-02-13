b2669fbba907701c85aa1c41a9f08773c23fce76
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-02-14 01:21:16 +0900

    엔드포인트 수정

--- src/main/java/com/messenger/web/MemberController.java
+++ src/main/java/com/messenger/web/MemberController.java
@@ -1,12 +1,10 @@
 package com.messenger.web;
 
 import com.messenger.domain.Member;
-import com.messenger.repository.MemberRepository;
 import com.messenger.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +15,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @Autowired
-    public MemberController(MemberRepository memberRepository, MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
@@ -25,10 +23,9 @@ public class MemberController {
      * 전체회원 목록
      * @return 전체회원 객체를 List로 반환
      */
-    @GetMapping("/api/members")
+    @GetMapping("/api/v1/members")
     public List<Member> members() {
-        List<Member> members = memberService.memberList();
-        return members;
+        return memberService.memberList();
     }
 
     /**
@@ -39,7 +36,7 @@ public class MemberController {
      * @return  정상적으로 가입된 경우 : 가입된 회원 객체
      *          그 외 : null
      */
-    @PostMapping(value = "/api/member/signup", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(value = "/api/v1/members", consumes = "application/x-www-form-urlencoded")
     public ResponseEntity<Member> signup(@RequestParam String id,
                                          @RequestParam String password,
                                          @RequestParam(required = false, defaultValue = "") String name) {
