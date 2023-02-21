b4b735ae206101920101d332a3b11b18a02e3246
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-02-21 15:07:41 +0900

    불변 클래스로 변경

--- src/main/java/com/messenger/domain/Member.java
+++ src/main/java/com/messenger/domain/Member.java
@@ -1,20 +1,15 @@
 package com.messenger.domain;
 
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter
+@Value
 @Builder(builderMethodName = "hiddenBuilder")
-@EqualsAndHashCode
-@ToString
 public class Member {
 
-    private final String id;
-    private String password;
-    @Builder.Default private String name = "undefined";
-    @Builder.Default private String statusMessage = "";  // 상태 메시지
+    String id;
+    String password;
+    @Builder.Default String name = "undefined";
+    @Builder.Default String statusMessage = "";  // 상태 메시지
 
     public static MemberBuilder builder(String id, String password) {
         return hiddenBuilder().id(id).password(password);
