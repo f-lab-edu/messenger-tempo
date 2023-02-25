0930fe1c56678477eb5b63aeff8b8d4976abeace
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-02-25 12:19:02 +0900

    @Repository 삭제 Member 클래스 필드 추가에 따른 수정

--- src/main/java/com/messenger/repository/JdbcMemberRepository.java
+++ src/main/java/com/messenger/repository/JdbcMemberRepository.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 @Slf4j
 public class JdbcMemberRepository implements MemberRepository {
 
@@ -26,7 +26,7 @@ public class JdbcMemberRepository implements MemberRepository {
      * @return 저장 성공 여부
      */
     public boolean save(Member member) {
-        String sql = "INSERT INTO member(id, pw, display_name) values(?, ?, ?)";
+        String sql = "INSERT INTO member(id, pw, display_name, status_message) values(?, ?, ?, ?)";
         Connection conn = null;
         PreparedStatement pstmt = null;
         try {
@@ -35,6 +35,7 @@ public class JdbcMemberRepository implements MemberRepository {
             pstmt.setString(1, member.getId());
             pstmt.setString(2, member.getPassword());
             pstmt.setString(3, member.getName());
+            pstmt.setString(4, member.getStatusMessage());
             pstmt.executeUpdate();
             return true;
         } catch (Exception e) {
@@ -180,11 +181,11 @@ public class JdbcMemberRepository implements MemberRepository {
      */
     @Override
     public boolean updateMember(Member paramMember) {
-        String sql = "UPDATE member SET pw = ?, display_name = ?, content = ? WHERE id = ?";
+        String sql = "UPDATE member SET pw = ?, display_name = ?, status_message = ? WHERE id = ?";
         Connection conn = null;
         PreparedStatement pstmt = null;
         try {
-            log.debug("UPDATE member SET pw = {}, display_name = {}, content = {} WHERE id = {}",
+            log.debug("UPDATE member SET pw = {}, display_name = {}, status_message = {} WHERE id = {}",
                     paramMember.getPassword(),
                     paramMember.getName(),
                     paramMember.getStatusMessage(),
