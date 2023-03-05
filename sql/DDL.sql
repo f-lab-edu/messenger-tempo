3ad8f4c587d4ab4cfa80eae7e3d63f4fbfe98c66
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-03-05 22:13:36 +0900

    1:1 채팅 테이블 DDL 작성

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -8,3 +8,14 @@ create table member(
     join_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
+
+create table chat_1on1 (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    message_from VARCHAR(35) NOT NULL,
+    message_to VARCHAR(35) NOT NULL,
+    message VARCHAR(5005),
+    unread_count TINYINT(1) DEFAULT 1,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    deleted BOOLEAN DEFAULT 0,
+    PRIMARY KEY(id)
+);
