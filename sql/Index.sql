ae6525529ac445beef5bb854182191c95c67c02e
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-03-16 20:45:17 +0900

    인덱스 추가

--- sql/Index.sql
+++ sql/Index.sql
@@ -1,1 +1,3 @@
-CREATE INDEX personal_chat_index ON personal_chat (group_id, id);
+CREATE INDEX idx_group_id ON personal_chat (group_id, id);
+CREATE INDEX idx_receiver_id ON personal_chat (receiver_user_id, id);
+CREATE INDEX idx_sender_id ON personal_chat (sender_user_id, id);
