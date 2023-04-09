5ac0278a27fa43e0dbe186bcbfdee93245bc6aee
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-04-09 15:16:35 +0900

    불필요한 파일 삭제

--- src/main/java/com/messenger/jwt/JwtProperties.java
+++
@@ -1,6 +0,0 @@
-//package com.messenger.jwt;
-//
-//public class JwtProperties {
-//    public static final String COOKIE_NAME = "jwt-authentication";
-//    public static final int EXPIRATION_TIME = 600_000;  // 10분
-//}
