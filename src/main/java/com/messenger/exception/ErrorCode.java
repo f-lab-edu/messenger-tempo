8cb2480e77c706e956a7bd92df1530c3645597da
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-04-10 12:55:57 +0900

    사용자 정의 에러 코드 추가

--- src/main/java/com/messenger/exception/ErrorCode.java
+++ src/main/java/com/messenger/exception/ErrorCode.java
@@ -10,8 +10,14 @@ public enum ErrorCode {
     FAIL_SIGNUP("회원가입 실패", HttpStatus.INTERNAL_SERVER_ERROR),
     ALREADY_EXIST_ID("이미 존재하는 아이디", HttpStatus.CONFLICT),
     ALREADY_LOGIN("이미 로그인 중인 사용자", HttpStatus.NOT_MODIFIED),
+    UNAUTHORIZED("권한이 없음", HttpStatus.UNAUTHORIZED),
     INTERNAL_SERVER_ERROR("서버 내부 에러", HttpStatus.INTERNAL_SERVER_ERROR),
-    OK("정상", HttpStatus.OK)
+    OK("정상", HttpStatus.OK),
+
+
+    FAIL_SAVE_CHAT("채팅 메시지 저장 실패", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAIL_DELETE_CHAT("잘못된 요청으로 메시지를 삭제할 수 없음", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_CHAT("채팅 메시지를 찾을 수 없음", HttpStatus.NOT_FOUND),
     ;
 
     public final String message;
