bcf7f27f8e0c3039e55eeb6263ef02ddc7360e90
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-04-14 23:26:17 +0900

    CLI 클라이언트 v2 구현 완성

--- CLI_client.py
+++ CLI_client.py
@@ -2,36 +2,43 @@ import requests
 import json
 
 URL_PREFIX = 'http://localhost:8080'
-url_mapping = {
+URL_MAPPING = {
+	'signup': ('/api/v1/members', 'POST', ['id', 'password', '-name']),
 	'login': ('/api/v1/members/login', 'POST', ['id', 'password']),
 	'list member': ('/api/v1/members', 'GET', []),
-	'signup': ('/api/v1/members', 'POST', ['id', 'password', '-name']),
 	
 	
-	'list chat by room': ('/api/v1/chat/personal_chat/{oppositeUserId}', 'GET', ['oppositeUserId', '-prevId', '-size']),
+	'list chat by room': ('/api/v1/chat/personal_chat/{oppositeUserId}', 'GET', ['oppositeUserId', '-nextId', '-size']),
 	'list room': ('/api/v1/chat/room', 'GET', []),
 	'enter room': ('/api/v1/chat/personal_chat/{oppositeUserId}/enter', 'GET', ['oppositeUserId', '-size']),
 	'get chat by id': ('/api/v1/chat/{chatId}', 'GET', ['chatId']),
 	'send chat': ('/api/v1/chat', 'POST', ['receiverUserId', 'content']),
 	'delete chat': ('/api/v1/chat/{chatId}', 'DELETE', ['chatId']),
-	'list all received chat': ('/api/v1/chat/received', 'GET', ['-prevId', '-size']),
-	'xxx': ('x', 'x', [])
+	'list all received chat': ('/api/v1/chat/received', 'GET', ['-nextId', '-size'])
 }
 
 global_cookie = dict()
 global_header = dict()
+# None, 'room list', 'in room'
+status = None
+global_opposite_user_id = None
+global_next_chat_id = None
 
-command_list = [
-	(None, None),  # not used index 0
-	('회원 가입', 'signup'),
-	('로그인', 'login'),
-	('방 목록', 'list room'),
-	('방 입장', 'enter room'),
-	('방 만들고 채팅 보내기', None),  # make new room and send chat
-	('채팅 메시지 보내기', 'send chat'),
-	('채팅 메시지 삭제', 'delete chat'),
-	('수신한 모든 채팅 메시지 목록', 'list all received chat')
-]
+COMMAND_LIST = {
+	'signup': 		('회원 가입', 'signup'),
+	'login': 		('로그인', 'login'),
+	'list room': 	('방 목록', 'list room'),
+	'enter room': 	('방 입장', 'enter room'),
+	'new room': 	('방 만들고 입장', None),
+	'list chat': 	('현재 방의 메시지 보기', 'list chat by room'),
+	'list more chat': ('메시지 더보기', 'list chat by room'),
+	'send chat': 	('채팅 메시지 보내기', 'send chat'),
+	'delete chat': 	('채팅 메시지 삭제', 'delete chat'),
+	'logout': 		('로그아웃', None),
+	'exit room': 	('방 나가기', 'list room'),
+	'list all received chat': ('수신한 모든 채팅 메시지 목록', 'list all received chat'),
+	'quit':			('종료', None)
+}
 
 
 def request_api(url, method = 'GET', payload = None):
@@ -54,7 +61,7 @@ def injection_pathvariable(url, param = None):
 
 
 def get_chat_by_id(chat_id):
-	full_url = URL_PREFIX + injection_pathvariable(url_mapping['get chat by id'][0], {'chatId': chat_id})
+	full_url = URL_PREFIX + injection_pathvariable(URL_MAPPING['get chat by id'][0], {'chatId': chat_id})
 	r = requests.request('GET', full_url, cookies=global_cookie, headers=global_header)
 	if not 200 <= r.status_code < 300:
 		print(f'[{r.status_code}] message={r.text}')
@@ -62,26 +69,44 @@ def get_chat_by_id(chat_id):
 	return json.loads(r.text).get('data')
 
 
-def prompt_command_list():
+def prompt_command_list(candidate):
 	print('-' * 30)
-	for i, command in enumerate(command_list):
-		if i == 0: continue
-		print(f'{i}) {command[0]}')
+	for i, t in enumerate(candidate):
+		print(f'{i+1}) {COMMAND_LIST[t][0]}')
 	print('-' * 30)
-	return
+	while True:
+		line = input('> ').rstrip().lower()
+		if line == 'q':
+			exit(0)
+		try:
+			command_idx = int(line)
+		except:
+			continue
+		
+		if not 1 <= command_idx <= len(candidate):
+			continue
+		break
+	return candidate[command_idx-1]
 
-def run_command(command, show = True):
-	url, method, param_li = url_mapping[command]
-	payload = dict()
+
+def run_command(command, payload = None, show = True):
+	global global_opposite_user_id
+	url, method, param_li = URL_MAPPING[command]
+	if payload is None:
+		payload = dict()
 	for param in param_li:
-		if param[0] != '-':
+		is_optional = param[0] == '-'
+		if is_optional:
+			param = param[1:]
+		if param in payload:
+			continue
+		if not is_optional:
 			while True:
 				val = input(f'파라미터 값 입력 (필수): {param}=').rstrip()
 				if val != "":
 					break
 			payload[param] = val
 		else:
-			param = param[1:]
 			val = input(f'파라미터 값 입력 (생략 가능): {param}=').rstrip()
 			if val != "":
 				payload[param] = val
@@ -91,14 +116,19 @@ def run_command(command, show = True):
 	# 헤더 출력
 	# if show:
 	#	print(json.dumps(header, indent=2))
-	if body_json is None:
-		return
 	
-	if show:
+	# 결과가 에러인 경우
+	if body_json is None:
+		return False
+	#if show:
 		# 바디 출력
-		print(json.dumps(body_json, indent=2))
-		# 깔끔한 출력
-		pretty_print(body_json, command)
+	#	print(json.dumps(body_json, indent=2))
+	# 깔끔한 출력
+	pretty_print(body_json, command)
+	
+	if command == 'enter room':
+		global_opposite_user_id = payload['oppositeUserId']
+		print("global_opposite_user_id=" + global_opposite_user_id)
 	
 	if 'Set-Cookie' in header:
 		temp = header['Set-Cookie']
@@ -110,47 +140,85 @@ def run_command(command, show = True):
 		token = header['Authorization']
 		print(token)
 		global_header['Authorization'] = token
-	return
+	return True
 
-def pretty_print(js, commmand):
+
+def pretty_print(js, command):
+	global global_next_chat_id
 	print('=' * 30)
 	if command == 'list room':
 		for t in js['data']:
 			opposite_user_id = t['first']
 			chat_id = t['second']
 			chat = get_chat_by_id(chat_id)
-			print(f"[1:1 채팅방] {opposite_user_id}와의 대화 / {chat['senderUserId']} : {chat['content']} (보낸 시간 : {chat['created_at']}, 읽은 시간 : {chat['read_at']})")
-	elif command in ['enter room', 'list all received chat']:
+			print(f"[1:1 채팅방] {opposite_user_id} 와(과)의 대화 / {chat['senderUserId']} : {chat['content']} (보낸 시간 : {chat['created_at']}, 읽은 시간 : {chat['read_at']})")
+	elif command in ['enter room', 'list chat by room', 'list all received chat']:
 		for chat in js['data']['list']:
 			print(f"[id={chat['id']}] {chat['senderUserId']} : {chat['content']} (보낸 시간 : {chat['created_at']}, 읽은 시간 : {chat['read_at']})")
+		global_next_chat_id = js['data']['nextId']
 	elif command == 'list member':
 		for member in js['data']:
 			print(f"id={member['id']}, name={member['name']}, status message={member['statusMessage']}")
-		
 	return
+
 
 if __name__ == "__main__":
 	print('커맨드를 번호로 입력')
 	print('종료할 때는 q 입력')
 	while True:
-		prompt_command_list()
-		line = input('> ').rstrip().lower()
-		if line == 'q':
-			break
-		try:
-			command_idx = int(line)
-		except:
-			continue
+		#print(f'global_header={global_header}')
+		#print(f'status={status}')
+		#print(f'global_opposite_user_id={global_opposite_user_id}')
+		candidate = []
+		if status is None:
+			candidate = ['signup', 'login', 'quit']
+		elif status == 'room list':
+			candidate = ['list room', 'enter room', 'new room', 'logout']
+		elif status == 'in room':
+			candidate = ['list chat', 'list more chat', 'send chat', 'delete chat', 'exit room']
 		
-		if not 1 <= command_idx <= len(command_list):
-			continue
+		selected_menu = prompt_command_list(candidate)
+		_, command = COMMAND_LIST[selected_menu]
 		
-		if command_list[command_idx][0] == '방 만들고 채팅 보내기':
-			run_command('list member', False)
-			run_command('send chat')
+		if selected_menu == 'quit':
+			exit(0)
+		elif selected_menu == 'new room':  # 방 만들고 입장
+			print('[유저 리스트]')
+			run_command('list member', show=False)
+			ret = run_command('enter room')	
+		elif command is not None:
+			if command == 'send chat' and global_opposite_user_id is not None:
+				ret = run_command(command, {'receiverUserId': global_opposite_user_id})
+			elif command == 'list chat by room' and global_opposite_user_id is not None:
+				payload = {'oppositeUserId': global_opposite_user_id}
+				if selected_menu == 'list more chat':
+					payload['nextId'] = global_next_chat_id
+				ret = run_command(command, payload)
+			else:
+				ret = run_command(command)
 		else:
-			_, command = command_list[command_idx]
-			run_command(command)
+			ret = True
+		
+		if not ret:
+			continue
+		
+		if status is None:
+			if selected_menu == 'login':
+				status = 'room list'
+				print('[방 목록]')
+				run_command('list room')
+		elif status == 'room list':
+			if selected_menu in ['enter room', 'new room']:
+				status = 'in room'
+			elif selected_menu == 'logout':
+				global_cookie.clear()
+				global_header.clear()
+				status = None
+		elif status == 'in room':
+			if selected_menu == 'exit room':
+				status = 'room list'
+				global_opposite_user_id = None
+				global_next_chat_id = None
 			
 		
 	
