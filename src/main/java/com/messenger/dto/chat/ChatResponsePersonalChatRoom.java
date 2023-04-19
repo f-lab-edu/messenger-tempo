353823fbb69c39218a888f21763364b7ad8180ba
tempo001 <83110819+tempo001@users.noreply.github.com>
2023-04-19 10:09:14 +0900

    Change: API 반환 형식 변경
    GET /api/v1/chat/room/{userId}
    GET /api/v1/chat/room

---
+++ src/main/java/com/messenger/dto/chat/ChatResponsePersonalChatRoom.java
@@ -0,0 +1,27 @@
+package com.messenger.dto.chat;
+
+import com.messenger.util.Pair;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.ToString;
+
+@Schema(description = "1:1 채팅방 ResponseDTO")
+@Getter
+@ToString
+public class ChatResponsePersonalChatRoom {
+
+    @Schema(description = "상대방 유저 id", defaultValue = "oppositeUserId")
+    private final String oppositeUserId;
+
+    @Schema(description = "마지막 채팅 id", defaultValue = "1")
+    private final Long lastChatId;
+
+    public ChatResponsePersonalChatRoom(String oppositeUserId, Long lastChatId) {
+        this.oppositeUserId = oppositeUserId;
+        this.lastChatId = lastChatId;
+    }
+
+    public static ChatResponsePersonalChatRoom of(Pair<String, Long> pair) {
+        return new ChatResponsePersonalChatRoom(pair.getFirst(), pair.getSecond());
+    }
+}
